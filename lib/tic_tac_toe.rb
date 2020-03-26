8b782045875e6593ab76edd4e03aa4b705d70ddd
Nick Martinez <nicolas.martinez.244@my.csun.edu>
2020-03-26 02:43:56 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -5,7 +5,7 @@ class TicTacToe
 
   WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], # Row wins
                       [0,3,6], [1,4,7], [2,5,8], # Col wins
-                      [0,4,8], [2,4,6]] # Diagonal wins 
+                      [0,4,8], [2,4,6]] # Diagonal wins
 
   def display_board(board)
     puts " #{board[0]} | #{board[1]} | #{board[2]} "
