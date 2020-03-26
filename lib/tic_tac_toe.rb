9733d2c01962e39f124d2a1212be2c4b361c5ac2
Nick Martinez <nicolas.martinez.244@my.csun.edu>
2020-03-26 02:51:04 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,6 +1,6 @@
 class TicTacToe
   def initialize(board = nil)
-    @board= board || Array.new(9, " ")
+    @board= Array.new(9, " ")
   end
 
   WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], # Row wins
