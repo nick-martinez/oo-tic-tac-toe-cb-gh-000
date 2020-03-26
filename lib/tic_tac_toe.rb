03756d6d140f589b369a7ebda2a3bfc21bfd18d2
Nick Martinez <nicolas.martinez.244@my.csun.edu>
2020-03-26 02:57:29 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -48,8 +48,8 @@ class TicTacToe
     end
   end # end valid_move
 
-  def current_player(board)
-    if (turn_count(board) % 2 == 0)
+  def current_player
+    if (turn_count % 2 == 0)
       return "X"
     else
       return "O"
